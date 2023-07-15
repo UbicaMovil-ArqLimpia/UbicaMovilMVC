cfa7710ca880fd4f55f20a746e1bc3297dcf4a88
Leonel Alexander <leo.gise02@outlook.com>
2023-07-15 10:00:04 -0600

    Revert "Revert "Se agrego: BL,DTOs,Interfaces,EN,EN Interfaces y DAL de categoria y se arreglaron algunas de empresa.""
    This reverts commit 2a05a9a8d030c7a69ac62c8226d140e439efc780.

---
+++ UbicaMovil.ArqLimpia.BL.DTOs/CategoriaDTOs/CategoriaGetAllDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UbicaMovil.ArqLimpia.BL.DTOs.CategoriaDTOs
+{
+    public class CategoriaGetAllDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UbicaMovil.ArqLimpia.DAL/CategoriaDAL.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UbicaMovil.ArqLimpia.EN.Interfaces;
+using UbicaMovil.ArqLimpia.DAL;
+using UbicaMovil.ArqLimpia.EN;
+
+
+namespace UbicaMovil.ArqLimpia.DAL
+{
+    public class CategoriaDAL : ICategoria
+    {
+        readonly UbicaMovilDBContext dbContext;
+        public CategoriaDAL(UbicaMovilDBContext Context)
+        {
+            dbContext = Context;
+        }
+
+        public void Create(Categoria categoria)
+        {
+            dbContext.Add(categoria);
+        }
+
+        public void Delete(Categoria categoria)
+        {
+            dbContext.Remove(categoria);
+        }
+
+        public async Task<List<Categoria>> GetAll()
+        {
+            return await dbContext.Categoria.ToListAsync();
+        }
+
+        public async Task<Categoria> GetById(int Id)
+        {
+            Categoria? categoria = await dbContext.Categoria.FirstOrDefaultAsync(s => s.Id == Id);
+            if (categoria != null)
+                return categoria;
+            else
+                return new Categoria();
+        }
+
+        public Task<List<Categoria>> Search(Categoria categoria)
+        {
+            var query = dbContext.Categoria.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(categoria.Name))
+                query = query.Where(s => s.Name.Contains(categoria.Name));
+            return query.ToListAsync();
+        }
+
+        public void Update(Categoria categoria)
+        {
+            dbContext.Update(categoria);
+        }
+    }
+}
