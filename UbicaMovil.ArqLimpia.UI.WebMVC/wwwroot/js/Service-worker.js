c6257326f83217314e70cec33fe2e2fe39fcb2f9
Leonel Alexander <leo.gise02@outlook.com>
2023-07-20 14:00:06 -0600

    Función para descargar y instalar

---
+++ UbicaMovil.ArqLimpia.UI.WebMVC/wwwroot/js/Service-worker.js
@@ -0,0 +1,23 @@
+﻿self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+
+        Promise.resolve()
+    );
+});
+
+self.addEventListener('activate', function (event) {
+
+    //realizar tarea de activacíón aqui
+});
+
+self.addEventListener('fetch', function (event) {
+
+    event.respondWith(
+        fetch(event.request)
+            .catch(function (error) {
+
+                console.log('error en la solicitud: ', error);
+            })
+    )
+}) 
---
+++ UbicaMovil.ArqLimpia.UI.WebMVC/wwwroot/js/install.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+let defferedInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+function saveBeforeInstallPromptEvent(evt) {
+
+	deferredInstallPrompt = evt;
+	installButton.removeAttribute('hidden');
+}
+
+function installPWA(evt) {
+
+	defferedInstallPrompt.prompt();
+	evt.srcElement.setAttribute('hidden', true);
+	defferedInstallPrompt.userChoice.then((choice) => {
+
+		if (choice.outcome === "accepted") {
+
+			console.log("Aceptado")
+
+		} else {
+			console.log("No aceptado")
+		}
+
+		defferedInstallPrompt = null;
+	})
+}
+
+window.addEventListener('appinstalled', logAppInstalled);
+
+function logAppInstalled(evt) {
+
+	console.log("Ubica Móvil Instalada");
+}
