e3064faae40874167e751b9bf065969fb2f3403b
Jacqueline Quinteros <giselaquinteros783@gmail.com>
2023-07-20 13:43:53 -0600

    Cambios y ya

--- UbicaMovil.ArqLimpia.DAL/Document/ScriptDB.sql
+++ UbicaMovil.ArqLimpia.DAL/Document/ScriptDB.sql
@@ -12,11 +12,11 @@ create table Empresas(
 Id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
 Nombre varchar(100) NOT NULL,
 Direccion varchar(Max)NOT NULL,
-Telefono varchar (10)NOT NULL,
+Telefono varchar (9)NOT NULL,
 HorarioEntrada varchar(8) NOT NULL,
 HorarioSalida varchar(8) NOT NULL,
-Latitud decimal NOT NULL,
-Longitud decimal NOT NULL,
+Latitud decimal(20,18) NOT NULL,
+Longitud decimal(20,18) NOT NULL,
 IdCategoria int NOT NULL,
 
 constraint FK_empresas_categorias foreign key(IdCategoria) references Categorias(Id)
